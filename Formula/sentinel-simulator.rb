7094a27379bc94699c5f63f0aac5373505552dbd
Peter Souter <petems@users.noreply.github.com>
2019-10-30 23:24:22 +0000

    Update sentinel-simulator.rb

--- Formula/sentinel-simulator.rb
+++ Formula/sentinel-simulator.rb
@@ -1,8 +1,11 @@
 class SentinelSimulator < Formula
   desc "Sentinel Simulator enables you to develop and test Sentinel policies locally on your own machine or in a CI environment."
   homepage 'https://docs.hashicorp.com/sentinel/'
-  url 'https://releases.hashicorp.com/sentinel/0.3.1/sentinel_0.3.1_darwin_amd64.zip'
-  sha256 'cd2d524c473f999191314b1a27b6fe6df2e14ec4345f9e9a1a5ce0f1ebb1e173'
+  
+  VERSION = '0.12.0'
+  
+  url "https://releases.hashicorp.com/sentinel/0.3.1/sentinel_#{VERSION}_darwin_amd64.zip"
+  sha256 '7d01196ff3a57bd142cbc9ab7550544a2ac7eb13865cec65da1bf9465dde6a53'
 
   def install
     bin.install 'sentinel'
